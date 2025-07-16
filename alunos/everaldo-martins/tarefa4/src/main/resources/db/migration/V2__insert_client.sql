5156cd3e4eb572f6c7f9f520010c150dc3b1a869
dev-emartins <everaldoinfortecnico@gmail.com>
2025-07-16 20:26:04 -0300

    feat:modificação da tarefa 4.

--- alunos/everaldo-martins/tarefa4/src/main/resources/db/migration/V2__insert_client.sql
+++ alunos/everaldo-martins/tarefa4/src/main/resources/db/migration/V2__insert_client.sql
@@ -1,7 +1,7 @@
 -- Script de migração para inserir dados iniciais na tabela clients
-INSERT INTO clients (id, name, email) VALUES
-    (RANDOM_UUID(), 'João Silva', 'joao.silva@example.com'),
-    (RANDOM_UUID(), 'Maria Souza', 'maria.souza@example.com'),
-    (RANDOM_UUID(), 'Carlos Oliveira', 'carlos.oliveira@example.com'),
-    (RANDOM_UUID(), 'Ana Santos', 'ana.santos@example.com'),
-    (RANDOM_UUID(), 'Pedro Costa', 'pedro.costa@example.com');
+INSERT INTO clients (name, email) VALUES
+    ('João Silva', 'joao.silva@example.com'),
+    ('Maria Souza', 'maria.souza@example.com'),
+    ('Carlos Oliveira', 'carlos.oliveira@example.com'),
+    ('Ana Santos', 'ana.santos@example.com'),
+    ('Pedro Costa', 'pedro.costa@example.com');
