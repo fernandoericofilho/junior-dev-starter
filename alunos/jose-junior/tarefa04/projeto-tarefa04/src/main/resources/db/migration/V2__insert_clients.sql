c5c56d9caf1e3b17ed5af01bd6ee940b2b413e89
José Júnior <59178745+josec-junior@users.noreply.github.com>
2025-07-18 12:04:13 -0300

    feat: adiciona tarefa04 - primeiro projeto no spring boot

---
+++ alunos/jose-junior/tarefa04/projeto-tarefa04/src/main/resources/db/migration/V2__insert_clients.sql
@@ -0,0 +1,8 @@
+-- Migration de inserção de dados na tabela Clients
+
+INSERT INTO clients (name, email) VALUES
+('Ana Silva', 'ana.silva@gmail.com'),
+('Bruno Costa', 'brunocosta@hotmail.com'),
+('Carlos Henrique', 'carlos.henrique@yahoo.com'),
+('Daniel Lacerda', 'danielacerda@outlook.com'),
+('José Júnior', 'josejunior.contato@gmail.com');
