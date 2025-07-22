fbeb9eff96ac3c3221d20391b40ff501e9430110
Eros Franklin <eros.felix@aluno.uepb.edu.br>
2025-07-22 13:50:52 -0300

    feat: adicionando tarefa 2 do exercicio 1

--- alunos/eros-franklin/tarefa-06/exercicios-java/EXERCICIO01.java/src/main/java/org/example/ConverterCelsiusFahrenheit.java
+++ alunos/eros-franklin/tarefa-06/exercicios-java/EXERCICIO01.java/src/main/java/org/example/ConverterCelsiusFahrenheit.java
@@ -1,7 +1,16 @@
 package org.example;
-
+import java.util.Scanner;
 public class ConverterCelsiusFahrenheit {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o valor da temperatura em Celsius:");
+        double var1 = scanner.nextDouble();
+
+        double fahrenheit = converterCelsius(var1);
+        System.out.println("O valor da temperatura" + var1 + " C em fahrenheit eh: " + fahrenheit);
+    }
+    public static double converterCelsius(double var1 ){
+        return (var1 * 1.8) + 32;
     }
 }
