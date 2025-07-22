f5848898a7d83c7c27af41f0fdeacefaeb675718
Eros Franklin <eros.felix@aluno.uepb.edu.br>
2025-07-22 14:11:00 -0300

    feat: adicionando tarefa5 do exercicio1

---
+++ alunos/eros-franklin/tarefa-06/exercicios-java/EXERCICIO01.java/src/main/java/org/example/ConstanteFinal.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class ConstanteFinal {
+    public static void main(String[] args) {
+        final int var1 = 1;
+        //final int var1 = 2;
+
+        //O java não altera o valor da var1 e imprime um erro no console, pois final
+        //é usado para variáveis com valores constantes
+    }
+}
