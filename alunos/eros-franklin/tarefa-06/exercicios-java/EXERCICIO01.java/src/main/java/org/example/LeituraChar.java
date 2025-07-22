606d3f8b89b9e1d259c1fe0fabbf7c60b011020a
Eros Franklin <eros.felix@aluno.uepb.edu.br>
2025-07-22 14:07:14 -0300

    feat: adicionando atividade4 do exercicio 1

---
+++ alunos/eros-franklin/tarefa-06/exercicios-java/EXERCICIO01.java/src/main/java/org/example/LeituraChar.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class LeituraChar {
+    public static void main(String[] args) {
+        //Nao sabia que dava pra fazer assim
+        char letra = 'A';
+        int codigo = (int) letra;
+
+        System.out.println("O caractere '" + letra + "' em ASCII eh: " + codigo);
+    }
+}
