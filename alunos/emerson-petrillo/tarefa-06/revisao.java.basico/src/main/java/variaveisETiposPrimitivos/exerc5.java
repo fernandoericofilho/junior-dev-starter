8e342e9ce0df4654493232fb59701c063e81007f
bobpetrillo <bobspetrillo@gmail.com>
2025-07-22 23:13:30 -0300

    feat: adiciona exercícios java

---
+++ alunos/emerson-petrillo/tarefa-06/revisao.java.basico/src/main/java/variaveisETiposPrimitivos/exerc5.java
@@ -0,0 +1,15 @@
+package variaveisETiposPrimitivos;
+
+public class exerc5 {
+
+    public static void main(String[] args) {
+
+        //Declare uma constante com final e tente alterá-la (observe o erro de compilação).
+
+        final int n = 10;
+
+        //n = 15;
+
+        System.out.println(n);
+    }
+}
