c3452c786f15ebb22e4d53f17046b9e5e1fbb0ce
Everaldo-Martins <everaldoinfortecnico@gmail.com>
2025-07-24 09:25:05 -0300

    feat:respostas dos exercicios (laços de repetição).

---
+++ alunos/everaldo-martins/exercicios-java/EXERCICIO18.java
@@ -0,0 +1,11 @@
+public class EXERCICIO18 {
+    public static void main(String[] args) {
+        int soma = 0;
+        
+        for(int i = 1; i <= 100; i++){
+            soma += i;
+        }
+
+        System.out.println("A soma de 1 a 100 é " + soma);
+    }
+}
