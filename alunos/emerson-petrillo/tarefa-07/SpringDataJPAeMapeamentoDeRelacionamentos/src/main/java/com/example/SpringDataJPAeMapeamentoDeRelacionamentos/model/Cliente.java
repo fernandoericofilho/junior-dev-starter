fff98025111249874d02d0d98d030aceb966f167
bobpetrillo <bobspetrillo@gmail.com>
2025-08-02 20:45:34 -0300

    feat: Adiciona nova tarefa 07

---
+++ alunos/emerson-petrillo/tarefa-07/SpringDataJPAeMapeamentoDeRelacionamentos/src/main/java/com/example/SpringDataJPAeMapeamentoDeRelacionamentos/model/Cliente.java
@@ -0,0 +1,97 @@
+package com.example.SpringDataJPAeMapeamentoDeRelacionamentos.model;
+
+import jakarta.persistence.*;
+import java.util.Objects;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "cliente")
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome", nullable = false)
+    private String nome;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @OneToOne(mappedBy = "cliente", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Endereco endereco;
+
+    @OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true)
+    private List<Telefone> telefones = new ArrayList<>();
+
+    public Cliente() {
+    }
+
+    public Cliente(String nome, String email) {
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public List<Telefone> getTelefones() {
+        return telefones;
+    }
+
+    public void setTelefones(List<Telefone> telefones) {
+        this.telefones = telefones;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cliente cliente = (Cliente) o;
+        return Objects.equals(id, cliente.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
---
+++ alunos/emerson-petrillo/tarefa-07/SpringDataJPAeMapeamentoDeRelacionamentos/src/test/java/com/example/SpringDataJPAeMapeamentoDeRelacionamentos/SpringDataJpAeMapeamentoDeRelacionamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringDataJPAeMapeamentoDeRelacionamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpAeMapeamentoDeRelacionamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
