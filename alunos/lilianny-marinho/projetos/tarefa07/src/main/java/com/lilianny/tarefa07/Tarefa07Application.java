4e7f379f5368167e2bec3aad03ad7958c9a8dc16
LiliannyMarinho <liliannymarinho@gmail.com>
2025-08-03 21:53:30 -0300

    feat: Organização

---
+++ alunos/lilianny-marinho/projetos/tarefa07/src/main/java/com/lilianny/tarefa07/Tarefa07Application.java
@@ -0,0 +1,13 @@
+package com.lilianny.tarefa07;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tarefa07Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tarefa07Application.class, args);
+	}
+
+}
---
+++ alunos/lilianny-marinho/projetos/tarefa07/src/test/java/com/lilianny/tarefa07/Tarefa07ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lilianny.tarefa07;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tarefa07ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
