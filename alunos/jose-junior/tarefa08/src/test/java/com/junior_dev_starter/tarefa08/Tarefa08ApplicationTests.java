1df6a5e0d68eb5d316f4f68a653ed84512d55735
José Júnior <59178745+josec-junior@users.noreply.github.com>
2025-09-30 15:49:32 -0300

    feat: implementa a resolução da tarefa 08

---
+++ alunos/jose-junior/tarefa08/src/test/java/com/junior_dev_starter/tarefa08/Tarefa08ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junior_dev_starter.tarefa08;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tarefa08ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
